0f9584a87b6f2b4690d486c9355ff8aaca2edb30
Taras <dmutruk.taras@gmail.com>
2022-03-12 14:33:58 +0200

    finish

--- src/layout/main.jsx
+++ src/layout/main.jsx
@@ -12,16 +12,23 @@ class Main extends React.Component {
     };
 
     componentDidMount() {
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=naruto`)
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=naruto`)
             .then((response) => response.json())
-            .then((data) => this.setState({ movies: data.Search, loading:false }));
+            .then((data) => this.setState({ movies: data.Search, loading:false }))
+            .catch((err)=>{
+                console.log(err);
+                this.setState({loading:false})
+            });
     }
 
     searchMovies = (text, filter) => {
         this.setState({loading:true})
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${text}&type=${filter}`)
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${text}&type=${filter}`)
             .then((response) => response.json())
-            .then((data) => this.setState({ movies: data.Search, loading:false }));
+            .then((data) => this.setState({ movies: data.Search, loading:false })).catch((err)=>{
+                console.log(err);
+                this.setState({loading:false})
+            });
     }
 
     render() {
